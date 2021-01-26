eef227744191ccf516385e1d86b6c5632bea86f1
DavidRMorphew <morphewd@umich.edu>
2021-01-25 20:32:27 -0500

    Done.

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,8 @@
 class Doctor < ApplicationRecord
     has_many :appointments
     has_many :patients, through: :appointments
+
+    def to_s
+        "#{self.name} — #{self.department}"
+    end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ApplicationRecord
     has_many :appointments
     has_many :doctors, through: :appointments
+
+    def to_s
+        "Name: #{self.name} Number of Appointments: #{self.appointments.count}"
+    end
 end
